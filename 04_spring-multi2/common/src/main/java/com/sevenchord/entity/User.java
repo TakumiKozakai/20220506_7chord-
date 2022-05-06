6badef4cfe7e27c1dc054dc69f9ac624c8b5ef8f
T.K <2b9sa2emil@gmail.com>
2022-05-06 19:05:21 +0900

    tmp

--- 04_spring-multi2/common/src/main/java/com/sevenchord/entity/User.java
+++ 04_spring-multi2/common/src/main/java/com/sevenchord/entity/User.java
@@ -1,8 +1,15 @@
 package com.sevenchord.entity;
 
-public class User {
-    
-    private String id;
-    
+import com.sevenchord.entity.base.BaseEntity;
 
+public class User extends BaseEntity {
+
+	private String id;
+	private String usernameKanji;
+	private String usernameKana;
+	private String mailAddress;
+	private String phoneNumber;
+	private String createdAt;
+	private String updatedAt;
+	private boolean deleted;
 }
